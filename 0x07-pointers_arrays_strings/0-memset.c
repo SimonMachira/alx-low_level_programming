585acecb0857acab66c94893ebe89c37e873e59c
SimonMachira <Simonmachira@gmail.com>
2023-10-24 09:46:59 -0700

    0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,18 +1,29 @@
 #include "main.h"
-/**
- * _memset - fill a block of memory with a specific value
- * @s: starting address of memory to be filled
- * @b: the desired value
- * @n: number of bytes to be changed
- * Return: changed array with new value for n bytes
- */
-char *_memset(char *s, char b, unsigned int n)
-{
-        unsigned int i;
+#include <stdio.h>
 
-        For (i = 0; i < n; i++)
-        {
-                s[i] = b;
-        }
-        return (s);
+/**
+* simple_print_buffer - prints buffer in hexa
+* @buffer: the address of memory to print
+* @size: the size of the memory to print
+* Return: Nothing.
+*/
+void simple_print_buffer(char *buffer, unsigned int size)
+{
+unsigned int i;
+
+	i = 0;
+	while (i < size)
+	{
+		if (i % 10)
+		{
+			printf(" ");
+		}
+		if (!(i % 10) && i)
+		{
+			printf("\n");
+		}
+		printf("0x%02x", buffer[i]);
+		i++;
+	}
+	printf("\n");
 }
