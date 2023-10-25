f5d1fa3af0e065e408fa1edb30350b8f9efb3758
SimonMachira <Simonmachira@gmail.com>
2023-10-24 17:09:37 -0700

    0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,15 +1,20 @@
-/**
-* main - check the code
-*
-* Return: Always 0.
-*/
-int main(void)
-{
-char buffer[98] = {0x00};
+#include "main.h"
 
-simple_print_buffer(buffer, 98);
-_memset(buffer, 0x01, 95);
-printf("-------------------------------------------------\n");
-simple_print_buffer(buffer, 98);
-return (0);
+/**
+ * _memset - ironnou
+ * @s: pointer to char params
+ * @b: data to change
+ * @n: index
+ * Return: *s
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		s[i] = b;
+	}
+	return (s);
 }
