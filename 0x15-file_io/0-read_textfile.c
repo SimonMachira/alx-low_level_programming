69c6e1e7d45d32503f084dfc30f90beb330250c4
SimonMachira <simonmachira@gmail.com>
2023-11-27 12:18:35 -0800

    0x15. C - File I/O

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcnt1.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+
+/**
+ * read_textfile - Reads and prints a text file to POSIX stdo
+ *
+ * @filename: name of file to read
+ *
+ * @letters: number of letters to read and print
+ *
+ * Return: number of letter read and print
+ */
+ssize_t read_textfile(const char *filename, size_t letters);
+{
+	int file_d;
+	ssize_t lens, lenw;
+	char *buffer;
+
+	if (filename == NULL)
+		return (0);
+	file_d = open(filename, O_RDONLY);
+	if (file_d == -1)
+		return (0);
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
+	{
+		close(file_d);
+		return (0);
+	}
+	lenr = read(file_d, buffer, letters);
+	close(file_d);
+	if (lenr == -1)
+	{
+		free(buffer);
+		return (0);
+	}
+	lenw = write(STDOUT FILENO, buffer, lenr);
+	free(buffer);
+	if (lenr != lenw)
+		return (0);
+	return (lenw);
+}
